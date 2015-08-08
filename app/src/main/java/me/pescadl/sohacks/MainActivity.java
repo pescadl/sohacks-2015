070dcb330d3455bf2ed995f284bf8347623fe2f3
chufanl <chufan.liang@gmail.com>
2015-08-08 00:31:23 -0500

    added wheel

--- app/src/main/java/me/pescadl/sohacks/MainActivity.java
+++ app/src/main/java/me/pescadl/sohacks/MainActivity.java
@@ -1,64 +1,65 @@
 package me.pescadl.sohacks;
 
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.animation.TimeInterpolator;
+import android.animation.ValueAnimator;
 import android.app.ActionBar;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.RectF;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener{
+    float currdegree;
+
+    private GestureDetectorCompat mDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDetector = new GestureDetectorCompat(this,this);
     }
 
     @Override
-    public void onResume(){
+    public void onResume() {
         super.onResume();
 
-        EventOpenHelper evHelper = new EventOpenHelper(getBaseContext());
-        SQLiteDatabase db = evHelper.getReadableDatabase();
-        String[] projection = {
-                "name",
-                "startTime",
-                "endTime",
-                "location",
-                "description"
-        };
-        String sortOrder = "name DESC";
-        Cursor c = db.query("events", projection, null, null, null, null, sortOrder);
-        c.moveToFirst();
-        TableLayout tab = (TableLayout) findViewById(R.id.eventTable);
-        tab.removeAllViews();
-        while(!c.isAfterLast()) {
-            TableRow tr = new TableRow(this);
-            TextView name = new TextView(this);
-            name.setText(c.getString(c.getColumnIndexOrThrow("name")));
-            TextView startTime = new TextView(this);
-            startTime.setText(c.getString(c.getColumnIndexOrThrow("startTime")));
-            TextView endTime = new TextView(this);
-            endTime.setText(c.getString(c.getColumnIndexOrThrow("endTime")));
-            TextView location = new TextView(this);
-            location.setText(c.getString(c.getColumnIndexOrThrow("location")));
-            TextView description = new TextView(this);
-            description.setText(c.getString(c.getColumnIndexOrThrow("description")));
-            tr.addView(name);
-            tr.addView(startTime);
-            tr.addView(endTime);
-            tr.addView(location);
-            tr.addView(description);
-            tab.addView(tr, new TableLayout.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT));
-            c.moveToNext();
-        }
+        Calendar c = Calendar.getInstance();
+        double decimalHours = c.get(Calendar.HOUR_OF_DAY) + c.get(Calendar.MINUTE) / 60.0;
+        float degrees = (int) ((decimalHours + 17) % 24 / 24.0 * 360.0);
+        ImageView wheelView = (ImageView) findViewById(R.id.wheelImageView);
+        currdegree = degrees;
+        wheelView.setRotation(-currdegree);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.mDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
     }
 
     @Override
@@ -86,5 +87,55 @@ public class MainActivity extends AppCompatActivity {
     public void createEvent(View v) {
         Intent intent = new Intent(this, CreateEventActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        if(flingAnimator != null){
+            flingAnimator.cancel();
+        }
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        ImageView wheelView = (ImageView) findViewById(R.id.wheelImageView);
+        currdegree += (distanceX)/9;
+        wheelView.setRotation(-currdegree);
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+    ValueAnimator flingAnimator;
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        ImageView wheelView = (ImageView) findViewById(R.id.wheelImageView);
+        flingAnimator = ValueAnimator.ofFloat(currdegree, currdegree-velocityX/20);
+        flingAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener(){
+
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                ImageView wheelView = (ImageView) findViewById(R.id.wheelImageView);
+                currdegree = (float)animation.getAnimatedValue();
+                wheelView.setRotation(-currdegree);
+            }
+        });
+        flingAnimator.setDuration(500);
+        flingAnimator.setInterpolator(new DecelerateInterpolator());
+        flingAnimator.start();
+        return false;
     }
 }
