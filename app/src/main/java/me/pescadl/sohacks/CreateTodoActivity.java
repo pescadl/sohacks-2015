afa5b34b7f6e8d9246f1ac237964bc4bcbf8f141
pescadl <dayounglee@utexas.edu>
2015-08-08 09:06:08 -0500

    added todo stuff

---
+++ app/src/main/java/me/pescadl/sohacks/CreateTodoActivity.java
@@ -0,0 +1,45 @@
+package me.pescadl.sohacks;
+
+import android.app.DialogFragment;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class CreateTodoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_todo);
+
+        findViewById(R.id.startDateText).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setStartDate(v);
+            }
+        });
+        findViewById(R.id.createTodoButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TodoOpenHelper evHelper = new TodoOpenHelper(getBaseContext());
+                SQLiteDatabase db = evHelper.getWritableDatabase();
+                ContentValues vals = new ContentValues();
+                vals.put("startTime", ((EditText) findViewById(R.id.startDateText)).getText().toString());
+                vals.put("description", ((EditText) findViewById(R.id.descriptionText)).getText().toString());
+                long newRowId;
+                newRowId = db.insert("todos", "null", vals);
+                db.close();
+                finish();
+            }
+        });
+    }
+
+    public void setStartDate(View v) {
+        DialogFragment newFragment = new StartDatePickerFragment();
+        newFragment.show(getFragmentManager(), "startDatePicker");
+    }
+
+}
